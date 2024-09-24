1de459e623be93a995e9bef75a83410ae0ef07ab
ShifaMittal050 <mstrialaccforcrm1@outlook.com>
2024-09-24 19:57:05 +0530

    Create resourcegroup.ps1

---
+++ IaC/AzCLI/resourcegroup.ps1
@@ -0,0 +1,9 @@
+#region Create Resource Group
+# This creates the resource group used to house the VM
+Write-Output "Creating resource group $resourceGroupName in region $resourceGroupNameRegion..."
+az group create `
+    --name $resourceGroupName `
+    --location $resourceGroupNameRegion
+    Write-Output "Done creating resource group"
+    Write-Output ""
+ #endregion
