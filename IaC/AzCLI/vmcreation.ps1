3205e881ad1331a94dbab391b376262174a7dfa5
ShifaMittal050 <mstrialaccforcrm1@outlook.com>
2024-09-24 19:56:46 +0530

    Update vmcreation.ps1

--- IaC/AzCLI/vmcreation.ps1
+++ IaC/AzCLI/vmcreation.ps1
@@ -63,15 +63,7 @@ Write-Output "Done"
 Write-Output ""
 #endregion
 
-#region Create Resource Group
-# This creates the resource group used to house the VM
-Write-Output "Creating resource group $resourceGroupName in region $resourceGroupNameRegion..."
-az group create `
-    --name $resourceGroupName `
-    --location $resourceGroupNameRegion
-    Write-Output "Done creating resource group"
-    Write-Output ""
- #endregion
+
 
 #region Create VM
 # Create a VM in the resource group
